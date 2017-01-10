dbbe348a4dd330e187039f2670d72f108e3b53eb
camil <kamildabrowka@gmail.com>
2017-01-10 19:14:14 +0100

    init

--- main.py
+++ main.py
@@ -1,20 +1,6 @@
 import io
-from newick import load
+from ete3 import Tree
 
+t = Tree('trees.txt')
 
-def print_descendants(node, depth):
-    print '  ' * depth + str(node.name) + ' : ' + str(node.length)
-    if len(node.descendants) > 0:
-        for desc in node.descendants:
-            print_descendants(desc, depth + 1)
-
-
-with io.open('trees.txt', encoding='utf8') as fp:
-    trees = load(fp)
-    print trees
-
-    for tree in trees:
-        depth_index = 0
-        print 'Tree\n'
-        print_descendants(tree, 0)
-        print '\n\n'
+print t
